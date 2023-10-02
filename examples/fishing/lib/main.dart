9b886b02081cc022f29945eb1604d2cd9313b729
Centny <centny@gmail.com>
2023-10-02 16:19:04 +0800

    add base test.

--- examples/fishing/lib/main.dart
+++ examples/fishing/lib/main.dart
@@ -1,125 +1,194 @@
+import 'dart:async';
+import 'dart:math' as math;
+
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flame/extensions.dart';
+import 'package:flame/input.dart';
+import 'package:flame/palette.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  maxTranslation = 50;
+  runApp(const GameWidget.controlled(gameFactory: FinshingGame.new));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+class MySpecialHitbox extends RectangleHitbox {
+  MySpecialHitbox() {
+    triggersParentCollision = false;
   }
+
+// hitbox specific onCollision* functions
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class FinshingGame extends FlameGame with PanDetector, TapCallbacks, KeyboardEvents, HasCollisionDetection {
+  late Player player;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Future<void> onLoad() async {
+    await super.onLoad();
+    player = Player();
+    var box = RectangleComponent(position: size / 2, size: Vector2(100, 100));
+    box.add(RectangleHitbox());
+    camera.viewport.add(FpsTextComponent());
+    world.add(player);
+    world.add(box);
+    world.addAll(createWalls());
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List<Component> createWalls() {
+    final view = camera.visibleWorldRect;
+    double wallOffset = 1;
+    double wallSize = 1000;
+    double x = view.width / 2 + wallSize / 2 - wallOffset;
+    double y = view.height / 2 + wallSize / 2 - wallOffset;
+    return [
+      Wall(direct: Vector2(1, 0), position: Vector2(-x, 0), size: Vector2(wallSize, view.height + 2 * wallSize)), //left
+      Wall(direct: Vector2(0, -1), position: Vector2(0, -y), size: Vector2(view.width + 2 * wallSize, wallSize)), //top
+      Wall(direct: Vector2(-1, 0), position: Vector2(x, 0), size: Vector2(wallSize, view.height + 2 * wallSize)), //right
+      Wall(direct: Vector2(0, 1), position: Vector2(0, y), size: Vector2(view.width + 2 * wallSize, wallSize)), //bottom
+    ];
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void onPanUpdate(DragUpdateInfo info) {
+    var p = camera.globalToLocal(info.eventPosition.game);
+    player.turnTo(p);
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    var p = camera.globalToLocal(event.canvasPosition);
+    player.turnTo(p);
+    player.fire();
+  }
+
+  @override
+  KeyEventResult onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    if (event.isKeyPressed(LogicalKeyboardKey.keyW)) {
+      player.nextWeapon();
+    }
+    return super.onKeyEvent(event, keysPressed);
+  }
+}
+
+class Wall extends RectangleComponent {
+  Vector2 direct;
+
+  Wall({required this.direct, super.position, super.size});
+
+  @override
+  FutureOr<void> onLoad() async {
+    await super.onLoad();
+    anchor = Anchor.center;
+    paint.color = Colors.yellow;
+    add(RectangleHitbox(anchor: Anchor.topLeft));
+  }
+}
+
+class Bullet extends CircleComponent with CollisionCallbacks {
+  final Vector2 direct;
+  final double speed;
+  final Color color;
+
+  Bullet({super.position, Vector2? direct, double? speed, Color? color})
+      : direct = direct ?? Vector2(0, 1),
+        speed = speed ?? 1000,
+        color = color ?? Colors.white;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    anchor = Anchor.center;
+    paint.color = color;
+    radius = 16;
+    add(CircleHitbox(radius: radius * 1.5));
+  }
+
+  @override
+  void update(double dt) {
+    position += direct * speed * dt;
+    super.update(dt);
+  }
+
+  @override
+  void onCollisionStart(Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollisionStart(intersectionPoints, other);
+    if (other is Wall) {
+      direct.reflect(other.direct);
+    }
+    // removeFromParent();
+  }
+}
+
+class Weapon extends RectangleComponent {
+  static List<Color> colors = [Colors.green.shade500, Colors.green.shade800, Colors.yellow.shade500, Colors.yellow.shade800, Colors.red.shade500, Colors.red.shade800];
+
+  int weapon = 0;
+  Vector2 direct = Vector2(0, 1);
+
+  @override
+  FutureOr<void> onLoad() async {
+    await super.onLoad();
+    paint.color = Weapon.colors[0];
+    anchor = Anchor.bottomCenter;
+    width = 16;
+    height = 50;
+    angle = 0;
+  }
+
+  void next() {
+    weapon = (weapon + 1) % Weapon.colors.length;
+    paint.color = Weapon.colors[weapon];
+  }
+
+  void turn(Vector2 v) {
+    var r = v.angleToSigned(Vector2(0, 1));
+    angle = math.pi - r;
+    direct = v.normalized();
+  }
+
+  Bullet createBullet(Vector2 position) {
+    var p = position + direct * height;
+    // log("new bullet on $p");
+    return Bullet(position: p, direct: direct, color: paint.color);
+  }
+}
+
+class Player extends RectangleComponent with HasGameReference<FinshingGame> {
+  Weapon weapon = Weapon();
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    paint.color = Colors.transparent;
+    anchor = Anchor.center;
+    width = 100;
+    height = 100;
+    position = Vector2(80 - game.size.x / 2, game.size.y / 2);
+    var center = Vector2(width / 2, height / 2);
+    weapon.position = center;
+    add(weapon);
+    add(CircleComponent(position: center, radius: 32, anchor: Anchor.center, paint: BasicPalette.white.paint()));
+  }
+
+  void move(Vector2 delta) {
+    position.add(delta);
+  }
+
+  void turnTo(Vector2 point) {
+    weapon.turn(point - position);
+  }
+
+  void nextWeapon() {
+    weapon.next();
+  }
+
+  void fire() {
+    game.world.add(weapon.createBullet(position));
   }
 }
